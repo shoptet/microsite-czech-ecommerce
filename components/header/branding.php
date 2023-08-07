3728945881b1a3eef6c6833d0629a718632b843c
Jakub Kolář <jk.oolar@gmailcom>
2023-08-07 17:51:49 +0200

    Change logos

--- components/header/branding.php
+++ components/header/branding.php
@@ -3,11 +3,11 @@
 	<div class="header__table">
 		<div class="header__cell header__cell--logos">
 			<div class="header__logo">
-                <a href="./"><img src="<?php echo THEME_WEB_ROOT; ?>/static/img/illust/content/logo-ceska-ecommerce.svg" alt="Česká e-commerce" width="99" height="28"></a>
+                <a href="./"><img src="<?php echo THEME_WEB_ROOT; ?>/static/img/illust/content/logo-ceska-ecommerce.svg" alt="Česká e-commerce" width="636" height="64"></a>
 			</div>
 		</div>
 		<div class="header__cell header__cell--claim">
-		    <a href="https://www.shoptet.cz/" target="_new"><img src="<?php echo THEME_WEB_ROOT; ?>/static/img/illust/content/logo-shoptet.png" alt="Shoptet" width="99" height="28"></a>
+		    <a href="https://www.shoptet.cz/" target="_new"><img src="<?php echo THEME_WEB_ROOT; ?>/static/img/illust/content/logo-shoptet.svg" alt="Shoptet" width="108" height="28"></a>
 				<span class="header__word pl-10 pr-10">a</span>
 				<a href="https://www.zbozi.cz/" rel="nofollow" target="_new"><img src="<?php echo THEME_WEB_ROOT; ?>/static/img/illust/content/logo-zbozi.png" alt="Zboží" width="136" height="34" class="header__img" class="header__img"></a>
 				<span class="header__word pl-10">přináší</span>
